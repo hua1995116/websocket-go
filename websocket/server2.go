33cf9e608922801d0c69d1021b7da7e3504c5111
huayifeng <461249104@qq.com>
2018-10-29 00:06:11 +0800

    featrure: go socket

---
+++ websocket/server2.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"time"
+	"net/http"
+	"github.com/gorilla/websocket"
+	"./impl"
+)
+
+var (
+	upgrader = websocket.Upgrader{
+		// 允许跨域
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+	}
+)
+
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+	var (
+		wsConn *websocket.Conn
+		err    error
+		data    []byte
+		conn *impl.Connection
+	)
+	// Upgrader header
+	if wsConn, err = upgrader.Upgrade(w, r, nil); err != nil {
+		return
+	}
+
+	if conn, err = impl.InitConnection(wsConn); err != nil {
+		goto ERR
+	}
+
+	go func () {
+		for{
+			if err = conn.WriteMessage([]byte("heartbeat")); err !=nil {
+				return
+			}
+			time.Sleep(1 * time.Second)
+		}
+	}()
+
+	for{
+		if data, err = conn.ReadMessage(); err!=nil {
+			goto ERR
+		}
+		if err = conn.WriteMessage(data);err!=nil {
+			goto ERR
+		}
+	}
+
+ERR:
+	conn.Close()
+}
+
+func main() {
+	http.HandleFunc("/ws", wsHandler)
+	http.ListenAndServe("0.0.0.0:7777", nil)
+}
